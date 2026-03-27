19220721b1da2fdd38383a02a3cbeb46b9617edc
Pulkit Mehta <mehta.pulkit94@gmail.com>
2026-03-27 21:58:49 +1100

    Vite typescript tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-amber-500">
+        <p className="m-10">Jelllo</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
